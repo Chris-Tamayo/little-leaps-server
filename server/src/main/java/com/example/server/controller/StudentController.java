6feced966f960ba55f97e1808cad515ed67b90a5
Jessie Jiang <jessie.jiang22@gmail.com>
2024-08-26 22:53:40 -0700

    Added new endpoint and fixed endpoint

--- server/src/main/java/com/example/server/controller/StudentController.java
+++ server/src/main/java/com/example/server/controller/StudentController.java
@@ -5,13 +5,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+    /**
+     * Returns the String "Student endpoint" when the default page is loaded.
+     * @return "Student endpoint" statement
+     */
     @GetMapping("/")
     public String getStudent() {
         return "Student endpoint";
     }
 
-    @GetMapping(/allStudents)
+    /**
+     * Returns "All students endpoint" after navigating to allStudents endpoint.
+     * @return "All students endpoint" statement
+     */
+    @GetMapping("/allStudents")
     public String getAllStudents() {
         return "All students endpoint";
+    }
+
+    /**
+     * Returns "Jessie Jiang" String after navigating to getName endpoint.
+     * @return "Jessie Jiang" statement
+     */
+    @GetMapping("/getName")
+    public String getName() {
+        return "Jessie Jiang";
     }
 }
